d785738e1406414389b03d4a4dd41e889218bfbf
Dmytro Naumchuk <dnaumchuk@playtika.com>
2019-08-09 19:17:13 +0300

    fuck commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux';
+
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from '../reducers';
+import rootSaga from '../sagas/sagas';
+
+const sagaMiddleWare = createSagaMiddleware();
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(sagaMiddleWare)
+    );
+
+sagaMiddleWare.run(rootSaga);
+
+export default store;
